896baeee0db832741b0f26d289592e97ff7348dc
Jonathan <monzalvearango@hotmail.com>
2019-11-12 10:18:42 -0500

    Actualización del index

--- view/index.php
+++ view/index.php
@@ -8,7 +8,7 @@
 
 	<section>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque iste ducimus totam, magnam neque reiciendis rerum quas mollitia laborum maiores, iure, harum hic cum! Sint quos nobis perferendis, suscipit totam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas debitis eius consectetur tempore corporis assumenda iure minus, possimus ab illo magni aperiam alias excepturi et quam dolorem tenetur neque perspiciatis!
+			esto es un parrafo
 		</p>
 	</section>
 
